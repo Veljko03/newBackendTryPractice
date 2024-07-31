de0443deee2490b427858b9ee3de214e6caa3b70
Veljko03 <veljkodovragovic@gmail.com>
2024-07-31 15:53:47 +0200

    port

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.post("/api/persons", (request, response) => {
   response.json(newNum);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
